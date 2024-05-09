408b6e88bcc76b65af1501727dac51b1d938dd83
HaroruDa3 <harol.kock34@gmail.com>
2024-05-09 12:05:06 -0600

    card finalizada

--- catracho-pizzas-app/src/app/product-card/product-card.component.ts
+++ catracho-pizzas-app/src/app/product-card/product-card.component.ts
@@ -1,14 +1,39 @@
 import { Component} from '@angular/core';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonAlert, IonIcon } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
-  standalone:true,
-  imports:[IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonAlert, IonIcon]
+  standalone: true,
+  imports: [
+    IonCard, IonCardHeader, IonCardTitle, IonCardContent, 
+    IonButton,  IonIcon
+  ]
 })
-export class ProductCardComponent  {
+export class ProductCardComponent {
+  count: number = 1;
 
-  constructor() { }
+  constructor(public alertController: AlertController) {}
 
+  increment() {
+    this.count++;
+  }
+
+  decrement() {
+    if (this.count > 1) {
+      this.count--;
+    }
+  }
+
+  async orderProduct() {
+    const alert = await this.alertController.create({
+      header: 'Confirmación',
+      message: 'Ha agregado correctamente el producto a su carrito',
+      buttons: ['Aceptar']
+    });
+
+    await alert.present();
+  }
 }
